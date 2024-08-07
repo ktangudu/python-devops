a9d25de17dd7ce9d76f14623b8dcfb0acfd808f7
ktangudu <ktangudu>
2024-08-07 23:25:16 +0530

    EXAMPLES

--- 01_variables.py
+++ 01_variables.py
@@ -5,7 +5,30 @@ This is Multi line comment
 
 '''
 #Define variable
-a = 42
+a = 42 #Integer variable
 
 # Print the value
 print(a) 
+
+'''
+-Boolean
+-string
+-float
+
+'''
+
+#Float value
+b = 42.5
+print(b)
+
+#Boolean
+c = True
+
+#Strings
+d = "This is a string"
+e = 'This is also a string'
+f = """This is multi line string"""
+
+#Today's weather
+d = 'Today's weather'
+print(d)
