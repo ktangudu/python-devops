9f940f87207bbf84887562d03fcb3508165798e1
ktangudu <ktangudu>
2024-08-07 23:39:53 +0530

    Further datatypes

--- 01_variables.py
+++ 01_variables.py
@@ -23,12 +23,27 @@ print(b)
 
 #Boolean
 c = True
+print(c)
 
 #Strings
 d = "This is a string"
 e = 'This is also a string'
 f = """This is multi line string"""
 
-#Today's weather
-d = 'Today's weather'
-print(d)
+
+#python specific datatypes
+test_list=["Hello","World","Python"]
+print(test_list)
+
+#Tuple
+test_tuple =("hello","world","python")
+print(test_tuple)
+
+#Dictionary
+test_dictionary = {'hello':1 ,'world':2,'python':3}
+print(test_dictionary)
+
+#set
+test_set = {'hello','world',"abc"}
+print(test_set)
+
